8a5a2eb9821b9826e0fa0611685c6a2cb2433776
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-06-27 21:42:04 +0200

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-    event.preventDefault();
+function displayPoem(response) {
 
-new Typewriter("#poem", {
-  strings: "Shall i compare thee to a summers day",
+ new Typewriter("#poem", {
+  strings: response.data.answer,
   autoStart: true,
   cursor: " ",
   delay: 10,
 });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let userInput = document.querySelector("#user-input");
+
+    let apiKey = "2c13e0a2b6fe347b0421bb02eef2o43t";
+    let context ="You are a talented romatic poem writer. You only write four line poems. You write every line with a <br /> at the bottom for the next line to continue. Sign the poem at the bottom left with SheCodes AI inside a <stong><strong/>";
+    let prompt = `Please write a poem about ${userInput}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem)
+
+   // console.log("Getting poem")
 
 }
 
