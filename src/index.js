4eb01698bbb52ae8047f35dd5ffb5f2f8b789e22
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-06-27 22:11:30 +0200

    Updated the context and prompt

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ function generatePoem(event) {
     let userInput = document.querySelector("#user-input");
 
     let apiKey = "2c13e0a2b6fe347b0421bb02eef2o43t";
-    let context ="You are a talented romatic poem writer. You only write four line poems. You write every line with a <br /> at the bottom for the next line to continue. Sign the poem at the bottom left with SheCodes AI inside a <stong><strong/>";
-    let prompt = `Please write a poem about ${userInput}`;
+    let context ="You are a talented romatic poem writer. You only write four line poems. Do not write general poems, use the given user instruction to write a poem about that thing. You write every line with a <br /> at the bottom for the next line to continue. Sign the poem at the bottom left with SheCodes AI inside a <stong><strong/>";
+    let prompt = `User instruction: Generate a poem about ${userInput}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     axios.get(apiUrl).then(displayPoem)
